6fc918a2a7f0ded08419b1fd504406e92f6af69a
emileerei <reiche2@rpi.edu>
2020-02-20 15:49:01 -0500

    Added a few more comments

--- transposedata.py
+++ transposedata.py
@@ -1,24 +1,24 @@
 import pandas as pd
 
+# transpose the total monthly data first
 df_m = pd.read_csv('mbta_total_monthly.csv', index_col='service_date')
 
 monthly = pd.DataFrame(columns=['date','value','mode'])
 
-for index in df_m.index:  #acts as for each date
-  for column in df_m.columns:   #acts as for each mode
+for index in df_m.index:  #for each date
+  for column in df_m.columns:   #for each mode of transport
     monthly = monthly.append({'date': index, 'value': df_m[column][index], 'mode': df_m[column].name}, ignore_index=True)
 
-# print(monthly)
 monthly.to_csv('updated_total.csv', index=False)
 
-
+# transpose the weekday average data next
 df_w = pd.read_csv('mbta_avg_nofloat.csv', index_col='service_date')
 
+# create an empty dataframe for our new data with 3 columns
 weekday = pd.DataFrame(columns=['date','value','mode'])
 
-for index in df_w.index:  #acts as for each date
-  for column in df_w.columns:   #acts as for each mode
+for index in df_w.index:  #for each date
+  for column in df_w.columns:   #for each mode of transport
     weekday = weekday.append({'date': index, 'value': df_w[column][index], 'mode': df_w[column].name}, ignore_index=True)
 
-# print(weekday)
 weekday.to_csv('updated_average.csv', index=False)
