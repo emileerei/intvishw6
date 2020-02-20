916e4fcc6b7e95902fd73f8e2e1761e5e9d19b3d
emileerei <reiche2@rpi.edu>
2020-02-20 11:56:07 -0500

    Updated color scheme and tooltips

--- index.js
+++ index.js
@@ -1,27 +1,28 @@
 chart("updated_total.csv", 1);
 chart("updated_average.csv", 2);
 
+
 var datearray = [];
 var colorrange = [];
 
 function chart(csvpath, chartNum) {
   // color scheme
   colorrange = [
-    "#741647", // commuter
-    "#e06666", // bus (bus)
-    "#d9d9d9", // bus (silver line)
-    "#333333", // RIDE
-    "#cc0000", // rail (red)
-    "#38761d", // rail (green)
-    "#6fa8dc", // rail (blue)
-    "#ff9900", // rail (orange)
-    "#0c343d" // ferry
+    "#5f4690", // commuter
+    "#edad08", // bus (bus)
+    "#666666", // bus (silver line)
+    "#994e95", // RIDE
+    "#cc503e", // rail (red)
+    "#0f8554", // rail (green)
+    "#1d6996", // rail (blue)
+    "#e17c05", // rail (orange)
+    "#38a6a5" // ferry
     ];
-  strokecolor = "#fff";
+  strokecolor = "#000";
 
   // replacing all .chart with corresponding class name
-  chartString = "";
-  if (chartNum == 1) {
+  var chartString = "";
+  if (chartNum === 1) {
     chartString = ".chart";
   } else {
     chartString = ".chart2";
@@ -31,7 +32,7 @@ function chart(csvpath, chartNum) {
   var format = d3.time.format("%Y-%m-%dT%H:%M:%S.%LZ");
 
   // svg/chart positioning
-  var margin = { top: 20, right: 90, bottom: 30, left: 70 };
+  var margin = { top: 40, right: 90, bottom: 30, left: 70 };
   var width = document.body.clientWidth - margin.left - margin.right;
   var height = 400 - margin.top - margin.bottom;
 
@@ -39,14 +40,23 @@ function chart(csvpath, chartNum) {
   var tooltip = d3.select("body")
     .append("div")
     .attr("class", "remove")
+    .style("background", "#fff")
+    .style("border", "1px solid #000")
+    .style("padding", "5px 20px 5px 20px")
     .style("position", "absolute")
     .style("z-index", "20")
     .style("visibility", "hidden")
-    .style("top", "30px")
+    .style("top", function () {
+      if (chartNum === 1) {
+        return "30px";
+      } else {
+        return height + 145 + "px";
+      }
+    })
     .style("left", "50px");
 
   var x = d3.time.scale()
-    .range([0, width]);
+    .range([0, 900]);
 
   var y = d3.scale.linear()
     .range([height - 10, 0]);
@@ -69,8 +79,7 @@ function chart(csvpath, chartNum) {
   var stack = d3.layout.stack()
     .offset("wiggle")
     .order(function (d) {
-      console.log(d);
-      return d3.range(d.length);
+      return [7, 6, 5, 4, 8, 2,3, 1, 0].reverse();
     })
     .values(function (d) { return d.values; })
     .x(function (d) { return d.date; })
@@ -91,12 +100,47 @@ function chart(csvpath, chartNum) {
     .append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
+  // legend creation
+  var legend = svg.selectAll('legend')
+    .data(colorrange)
+    .enter().append('g')
+    .attr('class', 'legend')
+    .style('z-index', '25')
+    .attr('transform', function (d, i) { return 'translate(1000,' + i * 30 + ')'; });
+
+  legend.append('rect')
+    .attr('y', 20)
+    .attr('width', 18)
+    .attr('height', 18)
+    .style('fill', function (d, i) { return colorrange[i]; });
+
+  legend.append("text")
+    .attr("x", 23)
+    .attr("y", 25)
+    .attr("dy", ".8em")
+    .style("text-anchor", "start")
+    .style("font-size", "10.5px")
+    .text(function (d, i) {
+      switch (i) {
+        case 0: return "Commuter Rail";
+        case 1: return "Bus (Bus)";
+        case 2: return "Bus (Silver Line)";
+        case 3: return "The RIDE";
+        case 4: return "Rail (Red)";
+        case 5: return "Rail (Green)";
+        case 6: return "Rail (Blue)";
+        case 7: return "Rail (Orange)";
+        case 8: return "Ferry (Boat-F1)";
+      }
+    });
+
   var graph = d3.csv(csvpath, function (data) {
     data.forEach(function (d) {
       d.date = format.parse(d.date);
       d.value = +d.value;
     });
 
+    // creation of streams
     var layers = stack(nest.entries(data));
 
     x.domain(d3.extent(data, function (d) { return d.date; }));
@@ -116,7 +160,7 @@ function chart(csvpath, chartNum) {
 
     svg.append("g")
       .attr("class", "y axis")
-      .attr("transform", "translate(" + width + ", 0)")
+      .attr("transform", "translate(" + 900 + ", 0)")
       .call(yAxis.orient("right"));
 
     svg.append("g")
@@ -127,12 +171,13 @@ function chart(csvpath, chartNum) {
       .attr("opacity", 1)
       .on("mouseover", function (d, i) {
         svg.selectAll(".layer").transition()
-          .duration(250)
+          .duration(300)
           .attr("opacity", function (d, j) {
-            return j != i ? 0.6 : 1;
+            return j != i ? 0.5 : 1;
           })
       })
 
+      // getting value to display as user mouses over graph
       .on("mousemove", function (d, i) {
         mousex = d3.mouse(this);
         mousex = mousex[0];
@@ -146,12 +191,13 @@ function chart(csvpath, chartNum) {
 
         mousedate = datearray.indexOf(invertedx);
         pro = d.values[mousedate].value;
+        var formatComma = d3.format(',');
 
         d3.select(this)
           .classed("hover", true)
           .attr("stroke", strokecolor)
           .attr("stroke-width", "0.5px"),
-          tooltip.html("<p>" + d.key + "<br>" + pro + "</p>").style("visibility", "visible");
+          tooltip.html("<p>Mode: " + d.key + "<br>Passenger Count:<br>" + formatComma(pro) + " Passengers</p>").style("visibility", "visible");
 
       })
       .on("mouseout", function (d, i) {
@@ -161,31 +207,7 @@ function chart(csvpath, chartNum) {
           .attr("opacity", "1");
         d3.select(this)
           .classed("hover", false)
-          .attr("stroke-width", "0px"), tooltip.html("<p>" + d.key + "<br>" + pro + "</p>").style("visibility", "hidden");
-      })
-
-    var vertical = d3.select(".chart")
-      .append("div")
-      .attr("class", "remove")
-      .style("position", "absolute")
-      .style("z-index", "19")
-      .style("width", "1px")
-      .style("height", "380px")
-      .style("top", "10px")
-      .style("bottom", "30px")
-      .style("left", "0px")
-      .style("background", "#fff");
-
-    d3.select(chartString)
-      .on("mousemove", function () {
-        mousex = d3.mouse(this);
-        mousex = mousex[0] + 5;
-        vertical.style("left", mousex + "px")
-      })
-      .on("mouseover", function () {
-        mousex = d3.mouse(this);
-        mousex = mousex[0] + 5;
-        vertical.style("left", mousex + "px")
+          .attr("stroke-width", "0px"), tooltip.html("<p>Mode: " + d.key + "<br>Passenger Count:" + pro + " Passengers</p>").style("visibility", "hidden");
       });
   });
 }
